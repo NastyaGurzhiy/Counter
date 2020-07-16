dad28d9686d5f6ce5467dbc615c01e5c85b79f32
Shary <a.gurzhyi@gmail.com>
2020-07-16 21:40:32 +0300

    pereimenovanie

--- Counter/Program.cs
+++ Counter/Program.cs
@@ -16,7 +16,7 @@ namespace Counter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MyWindow());
         }
     }
 }
